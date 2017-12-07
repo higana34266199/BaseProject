92a7cbd953043747238523beb70a94a9258e55a0
higana34266199 <j34266199@gmail.com>
2017-12-07 19:57:26 +0800

    yeah!!

---
+++ src/main/java/ash/DDD.java
@@ -0,0 +1,15 @@
+package ash;
+
+import java.util.Scanner;
+
+public class DDD {
+    public static void main(String[] args){
+        Scanner key = new Scanner(System.in);
+        int ash = key.nextInt();
+        if(ash != 0) {
+            System.out.println("WWWWWWWWWWW");
+        } else {
+            System.out.println("XD");
+        }
+    }
+}
---
+++ src/test/java/ash/DDDTest.java
@@ -0,0 +1,12 @@
+package ash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DDDTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+}
